13012905b234428d42483cdf1aa6a4378b0c3a96
noomnoose <69341012+noomnoose@users.noreply.github.com>
2021-06-22 09:51:40 +0700

    test ui

---
+++ Assets/color_ch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_ch : MonoBehaviour
+{
+    public Material _material;
+
+
+    public void SetColorRed()
+    {
+        _material.SetColor("_Color", Color.red);
+    }
+
+    public void SetColorGreen()
+    {
+        _material.SetColor("_Color", Color.green);
+    }
+
+    public void SetColorBlue()
+    {
+        _material.SetColor("_Color", Color.blue);
+    }
+}

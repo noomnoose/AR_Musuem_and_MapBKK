bcc964cae9761a284c73221e4f77c8ecd4f3c9ec
duckcud07 <68016578+duckcud07@users.noreply.github.com>
2021-06-24 14:15:25 +0700

    SceneChange_Script

---
+++ Assets/_PROJECT/SCRIPT/SceneChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public enum _scene 
+    {
+        _MAIN,
+        BKK_MAP_NEW,
+        Jar_sssFinal_Main
+    }
+    public _scene NextScene;
+    public void onClickNextScene()
+    {
+        SceneManager.LoadScene(NextScene.ToString());
+    }
+}

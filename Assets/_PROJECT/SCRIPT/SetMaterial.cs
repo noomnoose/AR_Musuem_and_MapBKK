310ff79c40309dd4a86b211e8cde7deef68bd9aa
noomnoose <69341012+noomnoose@users.noreply.github.com>
2021-07-09 16:57:23 +0700

    up set material

---
+++ Assets/_PROJECT/SCRIPT/SetMaterial.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterial : MonoBehaviour
+{
+
+    public Material[] material;
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+
+    public void Coloring()
+    {
+        rend.sharedMaterial = material[1];
+    }
+
+    public void Original()
+    {
+        rend.sharedMaterial = material[0];
+    }
+}

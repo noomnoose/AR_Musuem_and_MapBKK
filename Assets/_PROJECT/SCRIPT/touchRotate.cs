e2e93ba263a80c49b8fa71a0bf9f80ff161cf3b7
noomnoose <69341012+noomnoose@users.noreply.github.com>
2021-09-10 11:34:36 +0700

    update

--- Assets/_PROJECT/SCRIPT/touchRotate.cs
+++ Assets/_PROJECT/SCRIPT/touchRotate.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class touchRotate : MonoBehaviour
 {
+    public Quaternion startQuaternion;
+    public Vector3 quaternionToVcetor;
+    public Vector3 reverseQuaternion;
+    public float lerpTime = 1;
+    public float RotateAmount = 1;
+
+    public bool rotate;
+    public bool rotateConstantly;
+
+
+    private Touch touch;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startQuaternion = transform.rotation;
+        //quaternionToVcetor = startQuaternion.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                reverseQuaternion.x = +10;
+
+            }
+        }
+    
+        if (rotate)
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(reverseQuaternion), Time.deltaTime * lerpTime);
+        if (rotateConstantly && !rotate)
+            transform.Rotate(Vector3.up * RotateAmount);
     }
 }
